fe793047a7f50e4f7a73c3f9a132054f0165f78d
Martin P <nizu08@hotmail.com>
2021-11-19 21:29:40 +0200

    SignUpForm update

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -21,74 +21,91 @@ function SignUpForm() {
         marginTop: 8,
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
+        alignItems: 'left',
       }}
     >
-      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-{/*        TODO GIT ICON HERE
- *        <LockOutlinedIcon />*/}
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Sign Up
-      </Typography>
+      {/*<Grid container spacing={0} direction="column">*/}
+      <Box sx={{ ml: 5, minWidth: '100px' }}>
+        <Avatar sx={{ m: 1, ml:2 , bgcolor: 'secondary.main' }}>
+  {/*        TODO GIT ICON HERE
+    *        <LockOutlinedIcon />*/}
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+      </Box>
       {/*TODO HANDLE SUBMIT*/}
       {/*<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>*/}
       <Box component="form" noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          id="firstName"
-          label="First Name"
-          name="firstName"
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          id="lastName"
-          label="Last Name"
-          name="lastName"
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-        />
-        <TextField
-          margin="normal"
-          required
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-        />
-        <TextField
-          margin="normal"
-          required
-          name="passwordConfirm"
-          label="Confirm"
-          type="passwordConfirm"
-          id="passwordConfirm"
-        />
+        <div>
+          <TextField
+            margin="normal"
+            required
+            id="firstName"
+            label="First Name"
+            name="firstName"
+            autoFocus
+            sx={{ mr: 2, width: '25ch' }}
+          />
+          <TextField
+            margin="normal"
+            required
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            sx={{ mr: 2, width: '25ch' }}
+          />
+        </div>
+        <div>
+          <TextField
+            margin="normal"
+            required
+            id="email"
+            label="Email Address"
+            name="email"
+            sx={{ mr: 2, width: '100%', minWidth: '25ch', maxWidth: '447px' }}
+          />
+        </div>
+        <div>
+          <TextField
+            //helperText="Use 8 or more characters with a mix of letters and symbols"
+            margin="normal"
+            required
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            sx={{ mr: 2, width: '25ch' }}
+          />
+          <TextField
+            //helperText=" "
+            margin="normal"
+            required
+            name="passwordConfirm"
+            label="Confirm"
+            type="passwordConfirm"
+            id="passwordConfirm"
+            sx={{ mr: 2, width: '25ch' }}
+          />
+        </div>
         <Button
           type="submit"
           fullWidth
           variant="contained"
-          sx={{ mt: 3, mb: 2 }}
+          sx={{ mt: 3, mb: 5, width: '100px' }}
         >
           Sign Up
         </Button>
         <Grid container>
           <Grid item>
             <Link component={RouterLink} to="/login" variant="body2">
-              {"Already have an account? Log In Here!"}
+              {/*{"Already have an account? Log In Here!"}*/}
+              {"Log in instead"}
             </Link>
           </Grid>
         </Grid>
       </Box>
+      {/*</Grid>*/}
     </Box>
     )
 }
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,15 +1,16 @@
 import SignUpForm from '../components/SignUpForm';
+import Container from '@mui/material/Container';
 
 function SignUp() {
 
 
 
   return (
+    <Container>
+      <SignUpForm>
 
-    <SignUpForm>
-
-    </SignUpForm>
-    
+      </SignUpForm>
+    </Container>
     )
 }
 
