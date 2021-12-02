36ac9c78235e0ebf9033ba8e222cdb4f5fc2845f
Martin P <nizu08@hotmail.com>
2021-12-02 12:47:38 +0200

    mock API

---
+++ frontend/TESTING_TEMP_API/json-mock-api/readme.md
@@ -0,0 +1,10 @@
+
+
+https://www.pluralsight.com/guides/react-mock-api
+Mock API setup:
+
+npm install json-server --save
+
+npx json-server -p 3003 --watch src/db.json
+
+http://localhost:3003/tasks
--- frontend/src/webapp/actions.js
+++ frontend/src/webapp/actions.js
@@ -1,6 +1,7 @@
 export const USER_LOGIN = "USER_LOGIN"
 export const USER_LOGOUT = "USER_LOGOUT"
 
+//https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
 
 export const loginUser = data => ({
   type: USER_LOGIN,
@@ -10,3 +11,7 @@ export const loginUser = data => ({
 export const logoutUser = () => ({
   type: USER_LOGOUT
 })
+
+//export const getTasks = () => ({
+//  type: GET_TASKS
+//})
