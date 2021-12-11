15e3e8bd6ee29ad5c11ebeacf3f19e45980ac99c
Martin P <nizu08@hotmail.com>
2021-12-11 23:03:59 +0200

    AccountHeader refactor 2

--- frontend/src/components/AccountHeader.js
+++ frontend/src/components/AccountHeader.js
@@ -1,12 +1,23 @@
 import Grid from '@mui/material/Grid';
-
+import React, { useContext } from "react";
+import { Context } from "../webapp";
+import UserAvatar from './UserAvatar';
 
 function AccountHeader() {
+
+  const [state, setState] = useContext(Context);
+
+  console.log(state)
 
   return (
 
     <Grid container>
-      ACCOUNT HEADER HERE
+      <Grid item>
+        ACCOUNT HEADER HERE
+      </Grid>
+      <Grid item>
+        <UserAvatar />
+      </Grid>
     </Grid>
   )
 }
