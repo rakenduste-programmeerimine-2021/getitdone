e2aa3fc9fd7b174015942a24e016553c3305b35d
Martin P <nizu08@hotmail.com>
2021-12-15 20:28:15 +0200

    Update API.js

--- frontend/src/components/API.js
+++ frontend/src/components/API.js
@@ -5,7 +5,9 @@ export function addNewEvent(data) {
   //TODO event description
   axios.post('http://localhost:8080/api/event/create', data).then(resp => {
     console.log('New Event added')
-  });
+  }).catch(error => {
+    console.log(error)
+  })
 }
 
 export function editEvent(data) {
@@ -16,6 +18,30 @@ export function editEvent(data) {
     console.log(error)
   })
 }
+
+//TODO .. backend is broken
+export function addNewTask(data) {
+  axios.post('', data).then(resp => {
+    console.log('New Task added')
+  }).catch(error => {
+    console.log(error)
+  })
+}
+
+export function editTask(data) {
+  //TODO event description
+  axios.post('', data).then(resp => {
+    console.log('Edit Task')
+  }).catch(error => {
+    console.log(error)
+  })
+}
+
+  //.post("/createtask", taskController.createTask)
+  //.post("/gettask", taskController.getTaskDetails)
+  //.post("/changetaskdetails", taskController.changeTaskDetails)
+  //.post("/geteventtasks", taskController.getEventTasks)
+  //.post("/completetask", taskController.completeTask)
 
 //export function getEvents(uid, runAfter) {
 //  axios.get('http://localhost:3003/events').then(resp => {
